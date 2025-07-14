e0c5135f67ce00260a0ba744bebcd13602658c40
ujwal1441 <ujwal.r@s.amity.edu>
2025-07-14 11:48:03 +0530

    code of student details

---
+++ rec.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STUDENTS 5
+
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+void inputStudents(struct Student s[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter details for student %d\n", i+1);
